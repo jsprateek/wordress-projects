0db3696d26f9de31979c469823caf98613ae6542
Jarda Snajdr <jsnajdr@gmail.com>
2021-01-08 15:10:53 +0100

    Use sourceType:ambiguous when Babel-transpiling node_modules deps

--- babel.config.js
+++ babel.config.js
@@ -1,7 +1,18 @@
 module.exports = ( api ) => {
-	api.cache( true );
+	// When transpiling Gutenberg sources, always treat them as ESM modules.
+	// Helpers that Babel adds imports for will use ESM `import` syntax.
+	const sourceType = api.caller( ( caller ) => {
+		if ( [ 'WP_BUILD_MAIN', 'WP_BUILD_MODULE' ].includes( caller.name ) ) {
+			return 'module';
+		}
+
+		// When transpiling `node_modules` dependencies, the modules can be both ESM or CJS.
+		// Let Babel parser figure out the type from presence of `import`/`export` statements.
+		return 'unambiguous';
+	} );
 
 	return {
+		sourceType,
 		presets: [ '@wordpress/babel-preset-default' ],
 		plugins: [ 'babel-plugin-emotion', 'babel-plugin-inline-json-import' ],
 	};
